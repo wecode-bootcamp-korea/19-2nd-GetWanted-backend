9875d7e66893963102188730474e7517202840a8
dnjs0718 <mystund@naver.com>
2021-04-28 18:06:19 +0900

    Add: 구인공고 list views.py 작성완료 및 unit test 완료 - Q 객체 -> filter로 변경 - try except 구문 삭제 - unit test 코드 정렬 - notification에 해당하는 image 호출 방법 변경(불필요한 쿼리문 삭제) - 구인공고 필터링에 해당하는 tag view 작성 - tag view에 해당하는 unittest 완료 - 페이지네이션 구현 완료

--- companies/tests.py
+++ companies/tests.py
@@ -1,3 +1,69 @@
-from django.test import TestCase
+import json
 
-# Create your tests here.
+from django.test import TestCase,Client
+from .models     import Company,Notification,Image,Tag
+
+class NotificationTest(TestCase):
+
+    def setUp(self):
+        Company.objects.create(id=1,name='asdasds',address='dasdas',latitude=19.123123,longitude=22.231312)
+        Company.objects.create(id=2,name='asdasds',address='dasdas',latitude=19.123123,longitude=22.231312)
+        Company.objects.create(id=3,name='asdasds',address='dasdas',latitude=19.123123,longitude=22.231312)
+        Notification.objects.create(id=1,title='하이',description='dsadas',company=Company.objects.get(id=1))
+        Notification.objects.create(id=2,title='하이',description='dsadas',company=Company.objects.get(id=2))
+        Notification.objects.create(id=3,title='하이',description='dsadas',company=Company.objects.get(id=3))
+        Image.objects.create(id=1,image_url='sadasdasdasdas',notification=Notification.objects.get(id=1))
+        Image.objects.create(id=2,image_url='sadasdasdasdas',notification=Notification.objects.get(id=2))
+        Image.objects.create(id=3,image_url='sadasdasdasdas',notification=Notification.objects.get(id=3))
+
+    def tearDown(self):
+        Notification.objects.all().delete()
+        Company.objects.all().delete()
+        Image.objects.all().delete()
+    
+    def test_notification_list_get_success(self):
+        client=Client()
+        response = client.get('/notifications')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(),{
+            'notification_list':[{'title'   : '하이',
+                                  'image'   : 'sadasdasdasdas',
+                                  'id'      : 1,
+                                  'area'    : 'dasdas',
+                                  'company' : 'asdasds'},
+                                 {'title'   : '하이', 
+                                  'image'   : 'sadasdasdasdas', 
+                                  'id'      : 2, 
+                                  'area'    : 'da', 
+                                  'company' : 'asdasds'}, 
+                                 {'title'   : '하이', 
+                                  'image'   : 'sadasdasdasdas', 
+                                  'id'      : 3,
+                                  'area'    : 'da',
+                                  'company' : 'asdasds'}],
+                                  'total'   : 3})
+
+    def test_notification_list_get_tag_does_not_exist(self):
+        client=Client()
+        response = client.get('/notifications?tag=7')
+        self.assertEqual(response.json(),{'MESSAGE':'TAG_DOES_NOT_EXIST'})
+        self.assertEqual(response.status_code,404)
+
+class TagTest(TestCase):
+
+    def setUp(self):
+        Tag.objects.create(id=1,name='test1')
+        Tag.objects.create(id=2,name='test2')
+        Tag.objects.create(id=3,name='test3')
+
+    def tearDown(self):
+        Tag.objects.all().delete()
+
+    def test_tag_all_get_success(self):
+        client=Client()
+        response=client.get('/notifications/tag')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(),
+                        {'tag_list':[{'id' : 1, 'name' : 'test1'},
+                                     {'id' : 2, 'name' : 'test2'},
+                                     {'id' : 3, 'name' : 'test3'}]})
