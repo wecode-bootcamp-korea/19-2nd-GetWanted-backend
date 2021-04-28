a404e9de7f21c5fb7948a74799b5d49cfb091bf1
dnjs0718 <mystund@naver.com>
2021-04-28 21:29:24 +0900

    Add: companies/views.py 공고 detail 뷰 완성 및 Unit test 성공 - 불필요한 변수선언 및 쿼리 호출 삭제 - 불필요한 import 삭제

--- companies/tests.py
+++ companies/tests.py
@@ -1,69 +1,44 @@
 import json
 
 from django.test import TestCase,Client
-from .models     import Company,Notification,Image,Tag
+from .models     import Notification,NotificationTag,Image,Tag,Company
 
-class NotificationTest(TestCase):
-
+class NotificationDetailTest(TestCase):
     def setUp(self):
-        Company.objects.create(id=1,name='asdasds',address='dasdas',latitude=19.123123,longitude=22.231312)
-        Company.objects.create(id=2,name='asdasds',address='dasdas',latitude=19.123123,longitude=22.231312)
-        Company.objects.create(id=3,name='asdasds',address='dasdas',latitude=19.123123,longitude=22.231312)
-        Notification.objects.create(id=1,title='하이',description='dsadas',company=Company.objects.get(id=1))
-        Notification.objects.create(id=2,title='하이',description='dsadas',company=Company.objects.get(id=2))
-        Notification.objects.create(id=3,title='하이',description='dsadas',company=Company.objects.get(id=3))
-        Image.objects.create(id=1,image_url='sadasdasdasdas',notification=Notification.objects.get(id=1))
-        Image.objects.create(id=2,image_url='sadasdasdasdas',notification=Notification.objects.get(id=2))
-        Image.objects.create(id=3,image_url='sadasdasdasdas',notification=Notification.objects.get(id=3))
+        Company.objects.create(id=1,name='test1',address='서울 동대문구 한천로 63길 10',latitude=19.121212, longitude=20.12121)
+        Notification.objects.create(id=1,title='adasd',company=Company.objects.get(id=1),description='dasdas')
+        Image.objects.create(id=1,image_url='asdasdasdasdasdasdsa.jpg',notification=Notification.objects.get(id=1))
+        Image.objects.create(id=2,image_url='asdasdasdasdasdasdsa.jpg',notification=Notification.objects.get(id=1))
+        Tag.objects.create(id=1,name='하이하이')
+        Tag.objects.create(id=2,name='하이하이2')
+        NotificationTag.objects.create(id=1,notification=Notification.objects.get(id=1),tag=Tag.objects.get(id=1))
+        NotificationTag.objects.create(id=2,notification=Notification.objects.get(id=1),tag=Tag.objects.get(id=2))
 
     def tearDown(self):
-        Notification.objects.all().delete()
         Company.objects.all().delete()
-        Image.objects.all().delete()
-    
-    def test_notification_list_get_success(self):
+        Notification.objects.all().delete()
+
+    def test_notification_detail_get_success(self):
         client=Client()
-        response = client.get('/notifications')
+        response = client.get('/notifications/1')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json(),{
-            'notification_list':[{'title'   : '하이',
-                                  'image'   : 'sadasdasdasdas',
-                                  'id'      : 1,
-                                  'area'    : 'dasdas',
-                                  'company' : 'asdasds'},
-                                 {'title'   : '하이', 
-                                  'image'   : 'sadasdasdasdas', 
-                                  'id'      : 2, 
-                                  'area'    : 'da', 
-                                  'company' : 'asdasds'}, 
-                                 {'title'   : '하이', 
-                                  'image'   : 'sadasdasdasdas', 
-                                  'id'      : 3,
-                                  'area'    : 'da',
-                                  'company' : 'asdasds'}],
-                                  'total'   : 3})
-
-    def test_notification_list_get_tag_does_not_exist(self):
+        self.assertEqual(response.json(),{'NOTIFICATION_DETAIL': 
+        [{'notification_id': 1, 
+          'title'          : 'adasd', 
+          'description'    : 'dasdas',
+          'image_list'     : [{'id' : 1, 'image_url' : 'asdasdasdasdasdasdsa.jpg'}, 
+                              {'id' : 2, 'image_url' : 'asdasdasdasdasdasdsa.jpg'}], 
+          'tag_list'       : [{'id' : 1, 'name'      : '하이하이'}, 
+                              {'id' : 2, 'name'      : '하이하이2'}], 
+          'company_name'   : 'test1', 
+          'company_address': '서울 동대문구 한천로 63길 10', 
+          'company_area'   : '서울', 
+          'latitude'       : '19.1212120000', 
+          'longitude'      : '20.1212100000'
+          }]})
+          
+    def test_notification_detail_get_notification_does_not_exist(self):
         client=Client()
-        response = client.get('/notifications?tag=7')
-        self.assertEqual(response.json(),{'MESSAGE':'TAG_DOES_NOT_EXIST'})
-        self.assertEqual(response.status_code,404)
-
-class TagTest(TestCase):
-
-    def setUp(self):
-        Tag.objects.create(id=1,name='test1')
-        Tag.objects.create(id=2,name='test2')
-        Tag.objects.create(id=3,name='test3')
-
-    def tearDown(self):
-        Tag.objects.all().delete()
-
-    def test_tag_all_get_success(self):
-        client=Client()
-        response=client.get('/notifications/tag')
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json(),
-                        {'tag_list':[{'id' : 1, 'name' : 'test1'},
-                                     {'id' : 2, 'name' : 'test2'},
-                                     {'id' : 3, 'name' : 'test3'}]})
+        response = client.get('/notifications/555')
+        self.assertEqual(response.json(),{'MESSAGE':'NOTIFICATION_DOES_NOT_EXIST'})
+        self.assertEqual(response.status_code, 404)
--- companies/urls.py
+++ companies/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import NotificationView,TagView
+from .views      import NotificationView,TagView,NotificationDetailView
 
 urlpatterns = [
     path('',NotificationView.as_view()),
-    path('/tag',TagView.as_view())
+    path('/tag',TagView.as_view()),
+    path('/<int:notification_id>',NotificationDetailView.as_view())
 ]
