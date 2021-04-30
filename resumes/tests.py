1b5b725b06e2803863dda066bb494fb0c84c3088
leebj <redgpcmffld@gmail.com>
2021-04-30 20:55:30 +0900

    Add: resumes app ResumeView post method 추가 -작성한 이력서 저장 기능 추가 -code refactoring -tests.py success case 2개 추가

--- resumes/tests.py
+++ resumes/tests.py
@@ -1,3 +1,138 @@
-from django.test import TestCase
+import json, jwt
+
+from django.test import TestCase, Client
+
+from users.models import User, Position
+from .models import Resume,Career
+from my_settings import SECRET_KEY,algorithm
 
 # Create your tests here.
+
+class ResumeTest(TestCase):
+
+    def setUp(self):
+        Position.objects.create(name='frontend')
+        Position.objects.create(name='backend')
+        user = User.objects.create(
+            id       = 1,
+            email    = 'qwer@asdf.com',
+            password = 'asdfqwer',
+            position = Position.objects.get(name='backend')
+        )
+        Resume.objects.create(
+            title        = 'title',
+            name         = 'name',
+            email        = 'email',
+            phone_number = '000000000',
+            introduction = 'introduction',
+            status       = True,
+            user         = User.objects.get(id=1)
+        )
+        self.access_token = jwt.encode({'user_id': user.id}, SECRET_KEY, algorithm=algorithm)
+
+    def tearDown(self):
+        User.objects.all().delete()
+        Position.objects.all().delete()
+        Career.objects.all().delete()
+        Resume.objects.all().delete()
+
+    def test_resume_create_success(self):
+        client = Client()
+        header  = {'HTTP_Authorization': self.access_token}
+        request = {
+            'title'    : '제목',
+            'fullName' : '이름',
+            'email'    : 'asdf@asdf.com',
+            'phone'    : '전화번호',
+            'intro'    : '간단 소개글',
+            'isFinal'  : True,
+            'workInfo' : [
+                {
+                    "isWorking"   : False,
+                    "companyName" : "회사 이름",
+                    "startMonth"  : "12",
+                    "startYear"   : "2014",
+                    "endMonth"    : "01",
+                    "endYear"     : "2017",
+                    "position"    : "fkfkf",
+                    "details"     : "sdlflf"
+                },
+                {
+                    "isWorking"   : True,
+                    "companyName" : "2222",
+                    "startMonth"  : "2014",
+                    "startYear"   : "12",
+                    "position"    : "fffffff",
+                    "details"     : "dddddd"
+                },
+                {
+                    "isWorking"   : False,
+                    "companyName" : "3333",
+                    "startMonth"  : "12",
+                    "startYear"   : "2014",
+                    "endMonth"    : "01",
+                    "endYear"     : "2017",
+                    "position"    : "ffasdfff",
+                    "details"     : "ddsafdd"
+                }
+            ]
+        }
+        response = client.post('/resumes', json.dumps(request), **header, content_type='application/json')
+
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.json(),{'MESSAGE':'SUCCESS'})
+
+    def test_resume_create_success_empty_work_info(self):
+        client = Client()
+        header = {'HTTP_Authorization': self.access_token}
+        request = {
+            'title': '제목',
+            'fullName': '이름',
+            'email': 'asdf@asdf.com',
+            'phone': '전화번호',
+            'intro': '간단 소개글',
+            'isFinal': True
+        }
+        response = client.post('/resumes', json.dumps(request), **header, content_type='application/json')
+
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.json(), {'MESSAGE': 'SUCCESS'})
+
+    def test_resume_create_success_(self):
+        client = Client()
+        header = {'HTTP_Authorization': self.access_token}
+        request = {
+            'title': '제목',
+            'fullName': '이름',
+            'email': 'asdf@asdf.com',
+            'phone': '전화번호',
+            'isFinal': True,
+            'workInfo': [
+                {
+                    "isWorking": True,
+                    "companyName": "회사 이름",
+                    "startMonth": "12",
+                    "startYear": "2014",
+                    "position": "fkfkf",
+                    "details": "sdlflf"
+                }
+            ]
+        }
+        response = client.post('/resumes', json.dumps(request), **header, content_type='application/json')
+
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.json(), {'MESSAGE': 'SUCCESS'})
+
+    def test_key_error(self):
+        client = Client()
+        headers = {'HTTP_Authorization': self.access_token}
+        request = {
+            'fullName' : 'name',
+            'email'    : 'email',
+            'phone'    : '000000000000',
+            'intro'    : 'test'
+        }
+        response = client.post('/resumes', json.dumps(request), **headers, content_type='application/json')
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(),{'MESSAGE':'KEY_ERROR'})
--- resumes/views.py
+++ resumes/views.py
@@ -1,3 +1,59 @@
-from django.shortcuts import render
+import json
+
+from django.views import View
+from django.http  import JsonResponse,FileResponse
+
+from users.utils  import login_required
+
+from .models      import Resume,FileResume,Career
 
 # Create your views here.
+
+class ResumeView(View):
+    @login_required
+    def post(self,request):
+        try:
+            data         = json.loads(request.body)
+            resume_id    = data.get('resume_id')
+            careers_data = data.get('workInfo')
+
+            resume, created = Resume.objects.update_or_create(
+                id       = resume_id,
+                user     = request.user,
+                defaults = {
+                    'title'        : data['title'],
+                    'name'         : data['fullName'],
+                    'phone_number' : data['phone'],
+                    'email'        : data['email'],
+                    'introduction' : data.get('intro',''),
+                    'status'       : data.get('isFinal',0)
+                }
+            )
+            if careers_data:
+                for career_data in careers_data:
+                    is_working    = 0
+                    end_working   = ''
+                    start_working = f"{career_data.get('startYear')}-{career_data.get('startMonth')}"
+
+                    if career_data.get('isWorking'):
+                        is_working  = 1
+                        end_working = f"{career_data.get('endYear')}-{career_data.get('endMonth')}"
+
+                    Career.objects.update_or_create(
+                        id       = career_data.get('career_id'),
+                        resume   = resume,
+                        defaults = {
+                            'is_working'    : is_working,
+                            'start_working' : start_working,
+                            'end_working'   : end_working,
+                            'company_name'  : career_data.get('companyName',''),
+                            'department'    : career_data.get('position',''),
+                            'description'   : career_data.get('details','')
+                        }
+                    )
+                return JsonResponse({'MESSAGE':'SUCCESS'},status=201)
+            return JsonResponse({'MESSAGE':'SUCCESS'}, status=201)
+        except KeyError:
+            return JsonResponse({'MESSAGE':'KEY_ERROR'},status=400)
+        except ValueError:
+            return JsonResponse({'MESSAGE':'VALUE_ERROR'},status=400)
