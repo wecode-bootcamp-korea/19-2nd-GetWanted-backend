4494faad48d63335f0304646a344c4c7fe2d5fb0
leebj <redgpcmffld@gmail.com>
2021-05-03 21:30:16 +0900

    Add: resumes app ResumeView method get -작성한 이력서 불러오기 기능 추가 -작성할 이력서 기본 데이터 불러오기 기능 추가 -unit test 추가 -유저 정보 불러오기 기능 모듈화

--- resumes/tests.py
+++ resumes/tests.py
@@ -1,10 +1,11 @@
 import json, jwt
 
-from django.test import TestCase, Client
+from django.test  import TestCase, Client
 
 from users.models import User, Position
-from .models import Resume,Career
-from my_settings import SECRET_KEY,algorithm
+from .models      import Resume,Career
+
+from my_settings  import SECRET_KEY,algorithm
 
 # Create your tests here.
 
@@ -20,6 +21,7 @@ class ResumeTest(TestCase):
             position = Position.objects.get(name='backend')
         )
         Resume.objects.create(
+            id           = 1,
             title        = 'title',
             name         = 'name',
             email        = 'email',
@@ -27,6 +29,26 @@ class ResumeTest(TestCase):
             introduction = 'introduction',
             status       = True,
             user         = User.objects.get(id=1)
+        )
+        Resume.objects.create(
+            id           = 2,
+            title        = '제목',
+            name         = '이름',
+            email        = '이메일',
+            phone_number = '1111111111',
+            introduction = '자기소개',
+            status       = 1,
+            user         = User.objects.get(id=1)
+        )
+        Career.objects.create(
+            id            = 1,
+            is_working    = 0,
+            start_working = '2020-01',
+            end_working   = '2020-12',
+            company_name  = 'company_name',
+            department    = 'department',
+            description   = 'description',
+            resume        = Resume.objects.get(id=1)
         )
         self.access_token = jwt.encode({'user_id': user.id}, SECRET_KEY, algorithm=algorithm)
 
@@ -37,7 +59,7 @@ class ResumeTest(TestCase):
         Resume.objects.all().delete()
 
     def test_resume_create_success(self):
-        client = Client()
+        client  = Client()
         header  = {'HTTP_Authorization': self.access_token}
         request = {
             'title'    : '제목',
@@ -86,12 +108,12 @@ class ResumeTest(TestCase):
         client = Client()
         header = {'HTTP_Authorization': self.access_token}
         request = {
-            'title': '제목',
-            'fullName': '이름',
-            'email': 'asdf@asdf.com',
-            'phone': '전화번호',
-            'intro': '간단 소개글',
-            'isFinal': True
+            'title'    : '제목',
+            'fullName' : '이름',
+            'email'    : 'asdf@asdf.com',
+            'phone'    : '전화번호',
+            'intro'    : '간단 소개글',
+            'isFinal'  : True
         }
         response = client.post('/resumes', json.dumps(request), **header, content_type='application/json')
 
@@ -99,22 +121,22 @@ class ResumeTest(TestCase):
         self.assertEqual(response.json(), {'MESSAGE': 'SUCCESS'})
 
     def test_resume_create_success_(self):
-        client = Client()
-        header = {'HTTP_Authorization': self.access_token}
+        client  = Client()
+        header  = {'HTTP_Authorization': self.access_token}
         request = {
-            'title': '제목',
-            'fullName': '이름',
-            'email': 'asdf@asdf.com',
-            'phone': '전화번호',
-            'isFinal': True,
-            'workInfo': [
+            'title'    : '제목',
+            'fullName' : '이름',
+            'email'    : 'asdf@asdf.com',
+            'phone'    : '전화번호',
+            'isFinal'  : True,
+            'workInfo' : [
                 {
-                    "isWorking": True,
-                    "companyName": "회사 이름",
-                    "startMonth": "12",
-                    "startYear": "2014",
-                    "position": "fkfkf",
-                    "details": "sdlflf"
+                    "isWorking"   : True,
+                    "companyName" : "회사 이름",
+                    "startMonth"  : "12",
+                    "startYear"   : "2014",
+                    "position"    : "fkfkf",
+                    "details"     : "sdlflf"
                 }
             ]
         }
@@ -136,3 +158,86 @@ class ResumeTest(TestCase):
 
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response.json(),{'MESSAGE':'KEY_ERROR'})
+
+    def test_resume_get_success(self):
+        client = Client()
+        headers = {'HTTP_Authorization': self.access_token}
+
+        response = client.get('/resumes',**headers)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(),
+                         {'MESSAGE':'SUCCESS',
+                          'RESULTS':{
+                              'email'    : 'qwer@asdf.com',
+                              'fullName' : '',
+                              'phone'    : '',
+                              'title'    : '_3'}
+                          })
+
+    def test_existing_resume_get_all_success(self):
+        client = Client()
+        headers = {'HTTP_Authorization': self.access_token}
+
+        response = client.get('/resumes/1',**headers)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(),{
+            'MESSAGE':'SUCCESS',
+            'RESULTS':
+                {
+                    'email'    : 'email',
+                    'fullName' : 'name',
+                    'id'       : 1,
+                    'intro'    : 'introduction',
+                    'phone'    : '000000000',
+                    'title'    : 'title',
+                    'workInfo' : [{
+                        'id'          : 1,
+                        "isWorking"   : 0,
+                        "startYear"   : "2020",
+                        "startMonth"  : "01",
+                        "endYear"     : "2020",
+                        "endMonth"    : "12",
+                        "companyName" : "company_name",
+                        "position"    : "department",
+                        "details"     : "description"
+                    }
+                    ]}})
+
+    def test_existing_resume_get_empty_workinfo_success(self):
+        client = Client()
+        headers = {'HTTP_Authorization': self.access_token}
+
+        response = client.get('/resumes/2', **headers)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {
+            'MESSAGE':'SUCCESS',
+            'RESULTS':
+                {
+                    'email'    : '이메일',
+                    'fullName' : '이름',
+                    'id'       : 2,
+                    'intro'    : '자기소개',
+                    'phone'    : '1111111111',
+                    'title'    : '제목',
+                    'workInfo' : [
+                    ]}})
+
+    def test_invalid_resume_error(self):
+        client = Client()
+        headers = {'HTTP_Authorization': self.access_token}
+
+        response = client.get('/resumes/11',**headers)
+
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.json(),{'MESSAGE':'NOT_FOUND_RESUME'})
+
+    def test_invalid_values_resume_get_error(self):
+        client = Client()
+        headers = {'HTTP_Authorization': self.access_token}
+
+        response = client.get('/resumes/"ㅇㅇㅇ"',**headers)
+
+        self.assertEqual(response.status_code, 404)
--- resumes/urls.py
+++ resumes/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import ResumeView
+from .views      import ResumeView
 urlpatterns = [
+    path('/<int:resume_id>', ResumeView.as_view()),
     path('', ResumeView.as_view())
 ]
