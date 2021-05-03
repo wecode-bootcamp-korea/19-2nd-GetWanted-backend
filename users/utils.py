fcb782e25bdfc54cbd409b2e442dd53aa3378c54
deonii <fishingman99@gmail.com>
2021-05-03 18:45:12 +0900

    Modify: 소셜 로그인 구현(카카오, 네이버, 구글)  - 키값 통일  - 예외 상황 추가.  - tests.py 작성.  - setUpTestData 적용.

--- users/utils.py
+++ users/utils.py
@@ -1,9 +1,10 @@
 import jwt
 
-from django.http      import JsonResponse
-from users.models     import User
+from django.http       import JsonResponse
+from users.models      import User
+from users.validations import email_validation
 
-from my_settings      import algorithm,SECRET_KEY
+from my_settings       import algorithm,SECRET_KEY
 
 def login_required(func):
     def decorator(self, request, *args, **kwargs):
@@ -19,5 +20,18 @@ def login_required(func):
 
         except jwt.DecodeError:
             return JsonResponse({"MESSAGE": "INVALID_TOKEN"}, status=401)
-
+    
     return decorator
+
+def social_signin(name,email):
+    if not email_validation(email):
+        return 'INVALID_EMAIL'
+
+    if not User.objects.filter(email=email).exists():
+        User(name=name, email=email, position_id=1, is_social=1).save()
+
+    user = User.objects.get(email=email)
+
+    access_token = jwt.encode({'user_id': user.id}, SECRET_KEY, algorithm = algorithm)
+
+    return access_token
