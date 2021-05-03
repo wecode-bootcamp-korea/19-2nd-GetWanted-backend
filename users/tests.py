f353d0f32423b97d20500eb53a20c5b018894953
deonii <fishingman99@gmail.com>
2021-05-03 19:00:45 +0900

    Refactor: 컨벤션 수정.

--- users/tests.py
+++ users/tests.py
@@ -2,12 +2,13 @@ import jwt
 import json
 import bcrypt
 
-from django.test import TestCase
-from django.test import Client
+from django.test      import TestCase
+from django.test      import Client
 
-from companies.models import Company, Notification
-from users.models import User, Position, ApplyList
-from my_settings  import SECRET_KEY, algorithm
+from companies.models import Company, Notification, Image
+from users.models     import User, Position, ApplyList
+
+from my_settings      import SECRET_KEY, algorithm
 
 
 class SignUoTest(TestCase):
@@ -24,12 +25,12 @@ class SignUoTest(TestCase):
 
     def test_signup_post_success(self):
         client = Client()
-        user = {
-                'name' : '정재유',
-                'email' : 'fishingman99@naver.com',
-                'password' : '1q2w3e4r',
+        user   = {
+                'name'        : '정재유',
+                'email'       : 'fishingman99@naver.com',
+                'password'    : '1q2w3e4r',
                 'phonenumber' : '010-4444-5555',
-                'position' : '백엔드'
+                'position'    : '백엔드'
                 }
 
         response = client.post('/users/signup', json.dumps(user), content_type='application/json')
@@ -37,91 +38,91 @@ class SignUoTest(TestCase):
 
     def test_signup_post_fail_same_email(self):
         client = Client()
-        user = {
-                'name' : '정재유',
-                'email' : 'fishingman@naver.com',
-                'password' : '1q2w3e4r',
+        user   = {
+                'name'        : '정재유',
+                'email'       : 'fishingman@naver.com',
+                'password'    : '1q2w3e4r',
                 'phonenumber' : '010-4444-5555',
-                'position' : '백엔드'
+                'position'    : '백엔드'
                 }
 
         response = client.post('/users/signup', json.dumps(user), content_type='application/json')
         self.assertEqual(response.status_code, 401)
         self.assertEqual(response.json(),
                 {
-                    'MESSAGE': 'ALREADY_EXISTS_USER'
+                    'MESSAGE' : 'ALREADY_EXISTS_USER'
                 }
             )
 
     def test_signup_post_fail_email_validation(self):
         client = Client()
-        user = {
-                'name' : '정재유',
-                'email' : 'fishingmannaver.com',
-                'password' : '1q2w3e4r',
+        user   = {
+                'name'        : '정재유',
+                'email'       : 'fishingmannaver.com',
+                'password'    : '1q2w3e4r',
                 'phonenumber' : '010-4444-5555',
-                'position' : '백엔드'
+                'position'    : '백엔드'
                 }
 
         response = client.post('/users/signup', json.dumps(user), content_type='application/json')
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response.json(),
                 {
-                    'MESSAGE': 'INVALID_EMAIL'
+                    'MESSAGE' : 'INVALID_EMAIL'
                 }
             )
 
     def test_signup_post_fail_password_validation(self):
         client = Client()
-        user = {
-                'name' : '정재유',
-                'email' : 'fishingman99@naver.com',
-                'password' : '1q',
+        user   = {
+                'name'        : '정재유',
+                'email'       : 'fishingman99@naver.com',
+                'password'    : '1q',
                 'phonenumber' : '010-4444-5555',
-                'position' : '백엔드'
+                'position'    : '백엔드'
                 }
 
         response = client.post('/users/signup', json.dumps(user), content_type='application/json')
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response.json(),
                 {
-                    'MESSAGE': 'INVALID_PASSWORD'
+                    'MESSAGE' : 'INVALID_PASSWORD'
                 }
             )
 
     def test_signup_post_fail_phonenumber_validation(self):
         client = Client()
-        user = {
-                'name' : '정재유',
-                'email' : 'fishingman99@naver.com',
-                'password' : '1q2w3e4r',
+        user   = {
+                'name'        : '정재유',
+                'email'       : 'fishingman99@naver.com',
+                'password'    : '1q2w3e4r',
                 'phonenumber' : '01044445555',
-                'position' : '백엔드'
+                'position'    : '백엔드'
                 }
 
         response = client.post('/users/signup', json.dumps(user), content_type='application/json')
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response.json(),
                 {
-                    'MESSAGE': 'INVALID_NUMBER'
+                    'MESSAGE' : 'INVALID_NUMBER'
                 }
             )
 
     def test_signup_post_fail_keyerror(self):
         client = Client()
-        user = {
-                'name' : '정재유',
-                'account' : 'fishingmannaver.com',
-                'password' : '1q2w3e4r',
+        user   = {
+                'name'        : '정재유',
+                'account'     : 'fishingmannaver.com',
+                'password'    : '1q2w3e4r',
                 'phonenumber' : '010-4444-5555',
-                'position' : '백엔드'
+                'position'    : '백엔드'
                 }
 
         response = client.post('/users/signup', json.dumps(user), content_type='application/json')
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response.json(),
                 {
-                    'MESSAGE': 'KEY_ERROR'
+                    'MESSAGE' : 'KEY_ERROR'
                 }
             )
 
@@ -129,7 +130,13 @@ class EmailCheckTest(TestCase):
 
     def setUp(self):
         Position.objects.create(id=1, name='백엔드')
-        User.objects.create(name='정재유', email='fishingman99@naver.com', password='1q2w3e4r', phonenumber='010-4444-3333', position_id=1)
+        User.objects.create(
+                name        = '정재유', 
+                email       = 'fishingman99@naver.com', 
+                password    = '1q2w3e4r', 
+                phonenumber = '010-4444-3333', 
+                position_id = 1
+                )
 
     def tearDown(self):
         User.objects.all().delete()
@@ -137,7 +144,7 @@ class EmailCheckTest(TestCase):
 
     def test_email_check_success(self):
         client = Client()
-        email = {'email' : 'fishingman99@naver.com'}
+        email  = {'email' : 'fishingman99@naver.com'}
         
 
         response = client.post('/users/email', json.dumps(email), content_type='application/json')
@@ -145,13 +152,13 @@ class EmailCheckTest(TestCase):
 
     def test_email_check_fail_invalid_email(self):
         client = Client()
-        email = { 'email' :'fishingman99naver.com'}
+        email  = {'email' :'fishingman99naver.com'}
 
         response = client.post('/users/email', json.dumps(email), content_type='application/json')
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response.json(),
                 {
-                    'MESSAGE': 'INVALID_EMAIL'
+                    'MESSAGE' : 'INVALID_EMAIL'
                 }
             )
 
@@ -163,19 +170,19 @@ class EmailCheckTest(TestCase):
         self.assertEqual(response.status_code, 401)
         self.assertEqual(response.json(),
                 {
-                    'MESSAGE': 'ACCOUNT_NOT_EXIST'
+                    'MESSAGE' : 'ACCOUNT_NOT_EXIST'
                 }
             )
 
     def test_email_check_fail_keyerror(self):
         client = Client()
-        email = { 'account' :'fishingman99@naver.com'}
+        email  = {'account' : 'fishingman99@naver.com'}
 
         response = client.post('/users/email', json.dumps(email), content_type='application/json')
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response.json(),
                 {
-                    'MESSAGE': 'KEY_ERROR'
+                    'MESSAGE' : 'KEY_ERROR'
                 }
             )
 
@@ -183,13 +190,15 @@ class SignInCheckTest(TestCase):
 
     def setUp(self):
         client = Client()
-        Position.objects.create(id=1, name='백엔드')
+        Position.objects.create(
+                id   = 1, 
+                name = '백엔드')
         user = {
-                'name' : '정재유',
-                'email' : 'fishingman99@naver.com',
-                'password' : '1q2w3e4r',
+                'name'        : '정재유',
+                'email'       : 'fishingman99@naver.com',
+                'password'    : '1q2w3e4r',
                 'phonenumber' : '010-4444-5555',
-                'position' : '백엔드'
+                'position'    : '백엔드'
                 }
 
         client.post('/users/signup', json.dumps(user), content_type='application/json')
@@ -200,8 +209,8 @@ class SignInCheckTest(TestCase):
 
     def test_signin_check_success(self):
         client = Client()
-        user = {
-                'email' : 'fishingman99@naver.com',
+        user   = {
+                'email'    : 'fishingman99@naver.com',
                 'password' : '1q2w3e4r'
                 }
 
@@ -210,8 +219,8 @@ class SignInCheckTest(TestCase):
 
     def test_signin_check_fail_incorrect_password(self):
         client = Client()
-        user = {
-                'email' : 'fishingman99@naver.com',
+        user   = {
+                'email'    : 'fishingman99@naver.com',
                 'password' : '1q2w3e'
                 }
 
@@ -219,14 +228,14 @@ class SignInCheckTest(TestCase):
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response.json(),
                 {
-                    'MESSAGE': 'INCORRECT_PASSWORD'
+                    'MESSAGE' : 'INCORRECT_PASSWORD'
                 }
             )
 
     def test_signin_check_fail_keyerror(self):
         client = Client()
-        user = {
-                'account' : 'fishingman99@naver.com',
+        user   = {
+                'account'  : 'fishingman99@naver.com',
                 'password' : '1q2w3e4r'
                 }
 
@@ -234,7 +243,7 @@ class SignInCheckTest(TestCase):
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response.json(),
                 {
-                    'MESSAGE': 'KEY_ERROR'
+                    'MESSAGE' : 'KEY_ERROR'
                 }
             )
 
@@ -242,36 +251,41 @@ class ApplyviewTest(TestCase):
 
     def setUp(self):
         client = Client()
-        Position.objects.create(id=1, name='백엔드')
+        Position.objects.create(id = 1, name = '백엔드')
         User.objects.create(
-                id = 1,
-                name = '정재유',
-                email = 'fishingman99@naver.com',
-                password = '1q2w3e4r',
+                id          = 1,
+                name        = '정재유',
+                email       = 'fishingman99@naver.com',
+                password    = '1q2w3e4r',
                 phonenumber = '010-4444-5555',
                 position_id = 1)
 
         Company.objects.create(
-                id = 1, 
-                name = '이런회사', 
-                address = '서울', 
-                latitude = 124, 
+                id        = 1, 
+                name      = '이런회사', 
+                address   = '서울', 
+                latitude  = 124, 
                 longitude = 124
                 )
 
         Notification.objects.create(
-                id = 1,
-                title = '이런공고',
+                id          = 1,
+                title       = '이런공고',
                 description = '많이 지원바람', 
-                company_id = 1)
+                company_id  = 1)
 
         Notification.objects.create(
-                id = 5,
-                title = '이런공고입니다',
+                id          = 5,
+                title       = '이런공고입니다',
                 description = '많이 지원바람니다',
-                company_id = 1)
+                company_id  = 1)
 
-        ApplyList.objects.create(user_id=1, notification_id=5)
+        Image.objects.create(
+                id              = 1,
+                image_url       = 'www.naver.com',
+                notification_id = 5)
+
+        ApplyList.objects.create(user_id = 1, notification_id = 5)
 
         self.access_token = jwt.encode({'user_id': 1}, SECRET_KEY, algorithm = algorithm)
 
@@ -283,95 +297,96 @@ class ApplyviewTest(TestCase):
         ApplyList.objects.all().delete()
 
     def test_apply_check_success(self):
-        client = Client()
-        headers = { 'HTTP_Authorization' : self.access_token }
+        client   = Client()
+        headers  = { 'HTTP_Authorization' : self.access_token }
         response = client.get('/apply?notification=1', content_type='application/json', **headers)
 
         self.assertEqual(response.status_code, 200)
 
     def test_apply_fail_invalid_notification_id(self):
-        client = Client()
-        headers = { 'HTTP_Authorization' : self.access_token }
+        client   = Client()
+        headers  = { 'HTTP_Authorization' : self.access_token }
         response = client.get('/apply?notification=2', content_type='application/json', **headers)
 
         self.assertEqual(response.status_code, 401)
         self.assertEqual(response.json(),
                 {
-                    'MESSAGE': 'INVALID_NOTIFICATION_ID'
+                    'MESSAGE' : 'INVALID_NOTIFICATION_ID'
                 }
             )
 
     def test_apply_fail_already_apply(self):
-        client = Client()
-        headers = { 'HTTP_Authorization' : self.access_token }
+        client   = Client()
+        headers  = { 'HTTP_Authorization' : self.access_token }
         response = client.get('/apply?notification=5', content_type='application/json', **headers)
 
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response.json(),
                 {
-                    'MESSAGE': 'ALREADY_APPLY'
+                    'MESSAGE' : 'ALREADY_APPLY'
                 }
             )
 
     def test_apply_fail_keyerror(self):
-        client = Client()
-        headers = { 'HTTP_Authorization' : self.access_token }
+        client   = Client()
+        headers  = { 'HTTP_Authorization' : self.access_token }
         response = client.get('/apply?notificatio=1', content_type='application/json', **headers)
 
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response.json(),
                 {
-                    'MESSAGE': 'KEY_ERROR'
+                    'MESSAGE' : 'KEY_ERROR'
                 }
             )
 
     def test_apply_invalide_token(self):
-        client = Client()
-        headers = { 'HTTP_Authorization' : self.access_token+'11' }
+        client   = Client()
+        headers  = { 'HTTP_Authorization' : self.access_token+'11' }
         response = client.get('/apply?notification=1', content_type='application/json', **headers)
 
         self.assertEqual(response.status_code, 401)
         self.assertEqual(response.json(),
                 {
-                    'MESSAGE': 'INVALID_TOKEN'
+                    'MESSAGE' : 'INVALID_TOKEN'
                 }
             )
 
     def test_apply_invalide_login(self):
-        client = Client()
-        headers = { 'HTTP_Authorizate' : self.access_token }
+        client   = Client()
+        headers  = { 'HTTP_Authorizate' : self.access_token }
         response = client.get('/apply?notification=1', content_type='application/json', **headers)
 
         self.assertEqual(response.status_code, 401)
         self.assertEqual(response.json(),
                 {
-                    'MESSAGE': 'INVALID_LOGIN'
+                    'MESSAGE' : 'INVALID_LOGIN'
                 }
             )
 
     def test_apply_value_error(self):
-        client = Client()
-        headers = { 'HTTP_Authorization' : self.access_token }
+        client   = Client()
+        headers  = { 'HTTP_Authorization' : self.access_token }
         response = client.get('/apply?notification=answkduf', content_type='application/json', **headers)
 
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response.json(),
                 {
-                    'MESSAGE': 'VALUE_ERROR'
+                    'MESSAGE' : 'VALUE_ERROR'
                 }
             )
 
     def test_apply_success(self):
-        client = Client()
-        headers = { 'HTTP_Authorization' : self.access_token }
-        notification = { 'notification_id' : 1 }
+        client       = Client()
+        headers      = { 'HTTP_Authorization' : self.access_token }
+        notification = { 'notification_id': 1 }
+
         response = client.post('/apply', json.dumps(notification), content_type='application/json', **headers)
 
         self.assertEqual(response.status_code, 200)
 
     def test_applylist_check_success(self):
-        client = Client()
-        headers = { 'HTTP_Authorization' : self.access_token }
+        client   = Client()
+        headers  = { 'HTTP_Authorization' : self.access_token }
         response = client.get('/applylist', content_type='application/json', **headers)
 
         self.assertEqual(response.status_code, 200)
